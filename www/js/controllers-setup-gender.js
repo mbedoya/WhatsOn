b93a1ab79f80563903998be61b324f74f614146b
Mauricio <mauricio.bedoya@gmail.com>
2016-07-25 10:41:11 -0500

    User Data logic moved to services

--- www/js/controllers-setup-gender.js
+++ www/js/controllers-setup-gender.js
@@ -1,29 +1,12 @@
-controllersModule.controller('SetupGenderCtrl', function ($scope, $rootScope, $location, Utility) {
+controllersModule.controller('SetupGenderCtrl', function ($scope, $rootScope, $location, Utility, Thread) {
 
     $scope.selectGender = function(gender) {
 
-        var uid = firebase.database().ref().child('users').push().key;
-
-        var userData = { 
-            name: $rootScope.userProfile.name, 
-            gender: gender, 
-            creationTime: Utility.getCurrentDate() 
-        };
-
-        var updates = {};
-        updates['/users/' + uid] = userData;
-
-        firebase.database().ref().update(updates);
-
-        //Set User Data
-        $rootScope.userProfile.uid = uid;
+        $rootScope.showLoadingIndicator = true;
         $rootScope.userProfile.gender = gender;
-        $rootScope.userProfile.registered = true;
+        $rootScope.showLoadingIndicator = false;
 
-        //Storage User Data
-        localStorage.wo_name = $rootScope.userProfile.name;
-        localStorage.wo_uid = $rootScope.userProfile.uid;
-        localStorage.wo_gender = $rootScope.userProfile.gender;
+        User.register(gender);
 
         $location.path('/app/thread');
 
--- www/js/services.js
+++ www/js/services.js
@@ -21,7 +21,7 @@ angular.module('whatson.services', [])
 
                 firebase.database().ref().update(updates);
             },
-            AttachToChildren: function (fx, fxError) {
+            attachToChildren: function (fx, fxError) {
 
                 var recentPostsRef = firebase.database().ref('posts').limitToLast(100);
 
@@ -30,6 +30,39 @@ angular.module('whatson.services', [])
             }
         }
     })
+
+    .factory('User', function ($rootScope, Utility) {
+
+
+        return {
+            register: function () {
+
+                var uid = firebase.database().ref().child('users').push().key;
+
+                var userData = {
+                    name: $rootScope.userProfile.name,
+                    gender: $rootScope.userProfile.gender,
+                    creationTime: Utility.getCurrentDate()
+                };
+
+                var updates = {};
+                updates['/users/' + uid] = userData;
+
+                firebase.database().ref().update(updates);
+
+                //Set User Data
+                $rootScope.userProfile.uid = uid;
+                $rootScope.userProfile.registered = true;
+
+                //Storage User Data
+                localStorage.wo_name = $rootScope.userProfile.name;
+                localStorage.wo_uid = $rootScope.userProfile.uid;
+                localStorage.wo_gender = $rootScope.userProfile.gender;
+
+            }
+        }
+    })
+
     .factory('Utility', function () {
 
         this.padStr = function (i) {
